db8df2361851c14b8079aadfb97c7e458f25f7d6
XLP <1st@xlp-now.de>
2020-05-01 23:40:12 +0200

    Rename cpplintlist.txt to sourcelist.txt

--- lint.sh
+++ lint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-files=$(cat .ci/cpplintlist.txt | tr "\n" " ") # Read files to lint
+files=$(cat .ci/sourcelist.txt | tr "\n" " ") # Read files to lint
 filters=$(grep -o '^[^#]*' .ci/cpplintfilter.txt | tr "\n" ",") # read filters with periods instead of newlines
 filters=${filters// } # remove whitespaces
 cpplint --extensions=cpp,cxx,cc,c,c++ --headers=h,hpp,hxx --filter=$filters $files # lint with given filters
