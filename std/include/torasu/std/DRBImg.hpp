a592df7367612cd1c77244faf8dea2afa58abcca
XLP <1st@xlp-now.de>
2020-05-15 20:02:39 +0200

    DRBImg: Added format information

--- std/include/torasu/std/DRBImg.hpp
+++ std/include/torasu/std/DRBImg.hpp
@@ -4,8 +4,12 @@
 #include <string>
 
 #include <torasu/torasu.hpp>
+#include <torasu/DataPackable.hpp>
 
 namespace torasu::tstd {
+
+class DRBImg;
+class DRBImg_FORMAT;
 
 class DRBImg : public DataResource {
 private:
@@ -16,6 +20,7 @@ private:
 	uint64_t bufferSize;
 
 public:
+	explicit DRBImg(DRBImg_FORMAT format);
 	DRBImg(uint32_t width, uint32_t height);
 	virtual ~DRBImg();
 
@@ -26,6 +31,32 @@ public:
 	uint32_t getHeight();
 	uint64_t getBufferSize();
 	unsigned char* getImageData();
+
+
+};
+
+class DRBImg_FORMAT : public DataPackable {
+private:
+	const std::string formatIdent = std::string("STD::DRBIMG");
+	const std::string ident = std::string("STD::DPF_DRBIMG");
+
+	u_int32_t width, height;
+
+public:
+	explicit DRBImg_FORMAT(std::string jsonStripped);
+	explicit DRBImg_FORMAT(nlohmann::json jsonParsed);
+	DRBImg_FORMAT(u_int32_t width, u_int32_t height);
+
+	u_int32_t getWidth();
+	u_int32_t getHeight();
+
+	virtual std::string getIdent();
+	virtual void load();
+	virtual nlohmann::json makeJson();
+
+	inline ResultFormatSettings asFormat() {
+		return ResultFormatSettings(formatIdent, NULL, this);
+	}
 };
 
 } // namespace torasu::tstd
--- std/src/DRBImg.cpp
+++ std/src/DRBImg.cpp
@@ -2,13 +2,14 @@
 
 namespace torasu::tstd {
 
+DRBImg::DRBImg(DRBImg_FORMAT format) : DRBImg(format.getWidth(), format.getHeight()) {}
+
 DRBImg::DRBImg(uint32_t width, uint32_t height) {
 	this->width = width;
 	this->height = height;
 	this->bufferSize = width*height*4;
 	this->data = new uint8_t[bufferSize];
 }
-
 
 DRBImg::~DRBImg() {
 	delete[] data;
@@ -36,6 +37,48 @@ uint32_t DRBImg::getHeight() {
 
 uint8_t* DRBImg::getImageData() {
 	return data;
+}
+
+DRBImg_FORMAT::DRBImg_FORMAT(std::string jsonStripped) : DataPackable(jsonStripped) {}
+DRBImg_FORMAT::DRBImg_FORMAT(nlohmann::json jsonParsed) : DataPackable(jsonParsed) {}
+
+DRBImg_FORMAT::DRBImg_FORMAT(u_int32_t width, u_int32_t height) {
+	this->width = width;
+	this->height = height;
+}
+
+std::string DRBImg_FORMAT::getIdent() {
+	return ident;
+}
+
+void DRBImg_FORMAT::load() {
+	auto json = getJson();
+	if (json["w"].is_number()) {
+		width = json["w"];
+	} else {
+		width = 0;
+	}
+
+	if (json["h"].is_number()) {
+		height = json["h"];
+	} else {
+		height = 0;
+	}
+}
+
+nlohmann::json DRBImg_FORMAT::makeJson() {
+	return {
+		{"w", width},
+		{"h", height}
+	};
+}
+
+u_int32_t DRBImg_FORMAT::getWidth() {
+	return width;
+}
+
+u_int32_t DRBImg_FORMAT::getHeight() {
+	return height;
 }
 
 } // namespace torasu::tstd
