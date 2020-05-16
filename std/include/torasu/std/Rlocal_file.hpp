baf0f631c5b11a1a9d761b6dbfe3b2188f4349de
XLP <1st@xlp-now.de>
2020-05-16 20:17:09 +0200

    Added Rlocal_file

---
+++ std/include/torasu/std/Rlocal_file.hpp
@@ -0,0 +1,31 @@
+#ifndef STD_INCLUDE_TORASU_STD_RLOCAL_FILE_HPP_
+#define STD_INCLUDE_TORASU_STD_RLOCAL_FILE_HPP_
+
+#include <string>
+#include <torasu/torasu.hpp>
+#include <torasu/SimpleRenderable.hpp>
+#include <torasu/std/pipeline_names.hpp>
+#include <torasu/std/spoilsD.hpp>
+
+namespace torasu::tstd {
+
+class Rlocal_file : public tools::SimpleRenderable {
+private:
+	std::string pipeline = std::string(TORASU_STD_PL_FILE);
+
+	std::string path;
+
+protected:
+	virtual ResultSegment* renderSegment(ResultSegmentSettings* resSettings, RenderInstruction* ri);
+
+public:
+	explicit Rlocal_file(std::string path);
+	virtual ~Rlocal_file();
+
+	virtual DataResource* getData();
+	virtual void setData(DataResource* data);
+};
+
+} // namespace torasu::tstd
+
+#endif // STD_INCLUDE_TORASU_STD_RLOCAL_FILE_HPP_
---
+++ std/src/Rlocal_file.cpp
@@ -0,0 +1,51 @@
+#include "../include/torasu/std/Rlocal_file.hpp"
+
+#include <string>
+#include <fstream>
+
+#include <torasu/torasu.hpp>
+#include <torasu/std/Dfile.hpp>
+
+using namespace std;
+
+using namespace torasu::tools;
+
+namespace torasu::tstd {
+
+Rlocal_file::Rlocal_file(string path) : SimpleRenderable("STD::RLOCAL_FILE", true, false) {
+	this->path = path;
+}
+
+Rlocal_file::~Rlocal_file() {
+
+}
+
+ResultSegment* Rlocal_file::renderSegment(ResultSegmentSettings* resSettings, RenderInstruction* ri) {
+
+	if (resSettings->getPipeline().compare(pipeline) == 0) {
+
+		ifstream ifs(path, ios::binary|ios::ate);
+		ifstream::pos_type pos = ifs.tellg();
+
+		uint64_t size = pos;
+		Dfile* dfile = new Dfile(size);
+
+		char *pChars = (char*) (dfile->getFileData());
+		ifs.seekg(0, ios::beg);
+		ifs.read(pChars, size);
+
+		return new ResultSegment(ResultSegmentStatus_OK, dfile, false);
+	} else {
+		return new ResultSegment(ResultSegmentStatus_INVALID_SEGMENT);
+	}
+}
+
+DataResource* Rlocal_file::getData() {
+	return NULL; // TODO Rlocal_file getData
+}
+
+void Rlocal_file::setData(DataResource* data) {
+	// TODO Rlocal_file setData
+}
+
+} // namespace torasu::tstd
