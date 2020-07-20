08c852e7250276f1aac47ed1e04503efee5004e0
XLP <1st@xlp-now.de>
2020-07-20 16:01:14 +0200

    Added context-name for Duration

--- std/include/torasu/std/context_names.hpp
+++ std/include/torasu/std/context_names.hpp
@@ -5,4 +5,8 @@
  */
 #define TORASU_STD_CTX_TIME "STD::TIME"
 
+/** Duration in seconds (with decimal spaces)
+ */
+#define TORASU_STD_CTX_DURATION "STD::DUR"
+
 #endif // STD_INCLUDE_TORASU_STD_CONTEXT_NAMES_HPP_
