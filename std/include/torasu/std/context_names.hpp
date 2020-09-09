282befd2476682b20544bc6327563e15aedc6cc2
XLP <1st@xlp-now.de>
2020-09-09 12:25:15 +0200

    context_names: TORASU_STD_CTX_IMG_RATIO / types

--- std/include/torasu/std/context_names.hpp
+++ std/include/torasu/std/context_names.hpp
@@ -1,12 +1,19 @@
 #ifndef STD_INCLUDE_TORASU_STD_CONTEXT_NAMES_HPP_
 #define STD_INCLUDE_TORASU_STD_CONTEXT_NAMES_HPP_
 
-/** Time in seconds (with decimal spaces)
+/** Time in seconds
+ * type: torasu::tstd::Dnum
  */
 #define TORASU_STD_CTX_TIME "STD::TIME"
 
-/** Duration in seconds (with decimal spaces)
+/** Duration in seconds
+ * type: torasu::tstd::Dnum
  */
 #define TORASU_STD_CTX_DURATION "STD::DUR"
+
+/** Ratio of the imagery to be rendered width/height
+ * type: torasu::tstd::Dnum
+ */
+#define TORASU_STD_CTX_IMG_RATIO "STD::IMG_RATIO"
 
 #endif // STD_INCLUDE_TORASU_STD_CONTEXT_NAMES_HPP_
