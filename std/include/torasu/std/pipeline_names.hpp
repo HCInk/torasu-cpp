69dec1d46897b908a6b770bb34476ad9324c0d39
XLP <1st@xlp-now.de>
2020-09-15 10:01:11 +0200

    pipeline_names: Added default-types

--- std/include/torasu/std/pipeline_names.hpp
+++ std/include/torasu/std/pipeline_names.hpp
@@ -1,19 +1,34 @@
 #ifndef STD_INCLUDE_TORASU_STD_PIPELINE_NAMES_HPP_
 #define STD_INCLUDE_TORASU_STD_PIPELINE_NAMES_HPP_
 
-// Numeric-pipeline
+/**
+ * Numeric-pipeline
+ * Default-type: Dnum
+ */
 #define TORASU_STD_PL_NUM "STD::PNUM"
 
-// Visual-pipeline
+/**
+ * Visual-pipeline
+ * Default-type: Dbimg
+ */
 #define TORASU_STD_PL_VIS "STD::PVIS"
 
-// Audio-pipeline
+/**
+ * Audio-pipeline
+ * Default-type: Daudio_sequence
+ */
 #define TORASU_STD_PL_AUDIO "STD::PAUDIO"
 
-// File-pipeline
+/**
+ * File-pipeline
+ * Default-type: Dfile
+ */
 #define TORASU_STD_PL_FILE "STD::PFILE"
 
-// String-pipeline
+/** 
+ * bief: String-pipeline
+ * Default-type: Dstring
+ */
 #define TORASU_STD_PL_STRING "STD::PSTRING"
 
 #endif // STD_INCLUDE_TORASU_STD_PIPELINE_NAMES_HPP_
