e24d6091aff8e6844f20b103b786cf93d07d9c14
XLP <1st@xlp-now.de>
2020-12-17 23:30:35 +0100

    Reorganised Tests

--- examples/test/torasu-cpp-test.cpp
+++ examples/test/torasu-cpp-test.cpp
@@ -22,97 +22,102 @@
 using namespace torasu;
 using namespace torasu::tstd;
 
-TEST_CASE( "Simple numeric render test", "[simple-numeric]" ) {
-
-	// Creating "tree" to be rendered
-
-	Rnum rnum(1.1);
-
-	// Creating instruction
-
-	tools::RenderInstructionBuilder rib;
-
-	auto handle = rib.addSegmentWithHandle<Dnum>("STD::PNUM", NULL);
-
-	//	Create interface
-	
-	EIcore_runner runner;
-	ExecutionInterface* ei = runner.createInterface();
-
-	// Running render based on instruction
-
-	RenderContext rctx;
-
-	RenderResult* rr = rib.runRender(&rnum, &rctx, ei);
-
-	// Finding results
-
-	CHECK( rr->getStatus() == ResultStatus::ResultStatus_OK );
-	
-	if (rr->getStatus() == ResultStatus::ResultStatus_OK) {
-			
-		auto result = handle.getFrom(rr);
-
-		CHECK( result.getStatus() == ResultSegmentStatus::ResultSegmentStatus_OK );
-		
-		if (result.getStatus() == ResultSegmentStatus::ResultSegmentStatus_OK) {
-			CHECK( result.getResult()->getNum() == 1.1 );
-		}
-		
-	}
-
-	delete rr;
-
-	delete ei;
-
-}
-
 auto& IR = torasu::tools::inlineRenderable;
 
-TEST_CASE( "Advanced numeric render test", "[simple-numeric]" ) {
+struct SimpleNumeric {
 
-	// Creating "tree" to be rendered
+	void simpleNumeric() {
+		// Creating "tree" to be rendered
 
-	Rnum rnum(1.1);
-	Radd add(&rnum, 10);
-	Rmultiply mul(&add, IR( new Rsin(90) ));
+		Rnum rnum(1.1);
 
-	auto& tree = mul;
+		// Creating instruction
 
-	// Creating instruction
+		tools::RenderInstructionBuilder rib;
 
-	tools::RenderInstructionBuilder rib;
+		auto handle = rib.addSegmentWithHandle<Dnum>("STD::PNUM", NULL);
 
-	auto handle = rib.addSegmentWithHandle<Dnum>("STD::PNUM", NULL);
+		//	Create interface
+		
+		EIcore_runner runner;
+		ExecutionInterface* ei = runner.createInterface();
 
-	//	Create interface
-	
-	EIcore_runner runner;
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		// Running render based on instruction
 
-	// Running render based on instruction
+		RenderContext rctx;
 
-	RenderContext rctx;
+		RenderResult* rr = rib.runRender(&rnum, &rctx, ei);
 
-	std::unique_ptr<RenderResult> rr(rib.runRender(&tree, &rctx, ei.get()));
+		// Finding results
 
-	// Finding results
+		CHECK( rr->getStatus() == ResultStatus::ResultStatus_OK );
+		
+		if (rr->getStatus() == ResultStatus::ResultStatus_OK) {
+				
+			auto result = handle.getFrom(rr);
 
-	CHECK( rr->getStatus() == ResultStatus::ResultStatus_OK );
-	
-	if (rr->getStatus() == ResultStatus::ResultStatus_OK) {
+			CHECK( result.getStatus() == ResultSegmentStatus::ResultSegmentStatus_OK );
 			
-		auto result = handle.getFrom(rr.get());
-
-		CHECK( result.getStatus() == ResultSegmentStatus::ResultSegmentStatus_OK );
-		
-		if (result.getStatus() == ResultSegmentStatus::ResultSegmentStatus_OK) {
-			CHECK( result.getResult()->getNum() == 11.1 );
+			if (result.getStatus() == ResultSegmentStatus::ResultSegmentStatus_OK) {
+				CHECK( result.getResult()->getNum() == 1.1 );
+			}
+			
 		}
-		
+
+		delete rr;
+
+		delete ei;
+
 	}
 
-}
+	void advancedNumeric() {
+
+		// Creating "tree" to be rendered
+
+		Rnum rnum(1.1);
+		Radd add(&rnum, 10);
+		Rmultiply mul(&add, IR( new Rsin(90) ));
+
+		auto& tree = mul;
+
+		// Creating instruction
+
+		tools::RenderInstructionBuilder rib;
+
+		auto handle = rib.addSegmentWithHandle<Dnum>("STD::PNUM", NULL);
+
+		//	Create interface
+		
+		EIcore_runner runner;
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+
+		// Running render based on instruction
+
+		RenderContext rctx;
+
+		std::unique_ptr<RenderResult> rr(rib.runRender(&tree, &rctx, ei.get()));
+
+		// Finding results
+
+		CHECK( rr->getStatus() == ResultStatus::ResultStatus_OK );
+		
+		if (rr->getStatus() == ResultStatus::ResultStatus_OK) {
+				
+			auto result = handle.getFrom(rr.get());
+
+			CHECK( result.getStatus() == ResultSegmentStatus::ResultSegmentStatus_OK );
+			
+			if (result.getStatus() == ResultSegmentStatus::ResultSegmentStatus_OK) {
+				CHECK( result.getResult()->getNum() == 11.1 );
+			}
+			
+		}
+	}
+};
+
+METHOD_AS_TEST_CASE( SimpleNumeric::simpleNumeric , "Simple numeric render test", "[class]" )
+METHOD_AS_TEST_CASE( SimpleNumeric::advancedNumeric , "Advanced numeric render test", "[class]" )
+
 
 void numericBurstTest(ExecutionInterface* ei, size_t bursts, size_t perBurst) {
 
@@ -228,210 +233,198 @@ void numericBurstTestAsync(ExecutionInterface* ei, size_t bursts, size_t perBurs
 
 }
 
-TEST_CASE( "2K Numeric Burst (0-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner;
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTest(ei.get(), 2*1000, 1);
-
-}
-
-TEST_CASE( "2K Numeric burst (noQueue)", "[runner-test]" ) {
-	
-	EIcore_runner runner(false);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTest(ei.get(), 2*1000, 1);
-
-}
-
-
-TEST_CASE( "2K Numeric burst (noQueue, not concurrent)", "[runner-test]" ) {
-	
-	EIcore_runner runner(false, false);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTest(ei.get(), 2*1000, 1);
-
-}
-
-TEST_CASE( "2K Numeric burst (1-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner((size_t)1);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTest(ei.get(), 2*1000, 1);
-
-}
-
-TEST_CASE( "2K Numeric burst (2-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner((size_t)2);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTest(ei.get(), 2*1000, 1);
-
-}
-
-TEST_CASE( "2K Numeric burst (8-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner((size_t)8);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTest(ei.get(), 2*1000, 1);
-
-}
-
-TEST_CASE( "500*100 Numeric Burst (0-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner;
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTest(ei.get(), 500, 100);
-
-}
-
-TEST_CASE( "500*100 Numeric Burst (1-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner((size_t)1);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTest(ei.get(), 500, 100);
-
-}
-
-TEST_CASE( "500*100 Numeric Burst (2-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner((size_t)2);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTest(ei.get(), 500, 100);
-
-}
-
-TEST_CASE( "500*100 Numeric Burst (8-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner((size_t)8);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTest(ei.get(), 500, 100);
-
-}
-
-TEST_CASE( "500*100*1 Numeric Burst (0-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner;
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTestAsync(ei.get(), 500, 100, 1);
-
-}
-
-TEST_CASE( "500*10*10 Numeric Burst (0-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner;
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTestAsync(ei.get(), 500, 10, 10);
-
-}
-
-TEST_CASE( "500*10*10 Numeric Burst (1-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner((size_t)1);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTestAsync(ei.get(), 500, 10, 10);
-
-}
-
-TEST_CASE( "500*10*10 Numeric Burst (2-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner((size_t)2);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTestAsync(ei.get(), 500, 10, 10);
-
-}
-
-TEST_CASE( "500*10*10 Numeric Burst (8-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner((size_t)8);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTestAsync(ei.get(), 500, 10, 10);
-
-}
-
-TEST_CASE( "50*10*100 Numeric Burst (0-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner;
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTestAsync(ei.get(), 50, 10, 100);
-
-}
-
-TEST_CASE( "50*10*100 Numeric Burst (1-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner((size_t)1);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTestAsync(ei.get(), 50, 10, 100);
-
-}
-
-TEST_CASE( "50*10*100 Numeric Burst (2-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner((size_t)2);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTestAsync(ei.get(), 50, 10, 100);
-
-}
-
-TEST_CASE( "50*10*100 Numeric Burst (8-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner((size_t)8);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTestAsync(ei.get(), 50, 10, 100);
-
-}
-
-TEST_CASE( "50*100*100 Numeric Burst (0-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner;
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTestAsync(ei.get(), 50, 100, 100);
-
-}
-
-TEST_CASE( "50*100*100 Numeric Burst (1-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner((size_t)1);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTestAsync(ei.get(), 50, 100, 100);
-
-}
-
-TEST_CASE( "50*100*100 Numeric Burst (2-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner((size_t)2);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTestAsync(ei.get(), 50, 100, 100);
-
-}
-
-TEST_CASE( "50*100*100 Numeric Burst (8-thread)", "[runner-test]" ) {
-	
-	EIcore_runner runner((size_t)8);
-	std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
-
-	numericBurstTestAsync(ei.get(), 50, 100, 100);
-
-}
+struct RunnerTest {
+	// 2K Numeric Burst (0-thread)
+	void b_2K_0t() {
+		EIcore_runner runner;
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTest(ei.get(), 2*1000, 1);
+	}
+
+	// 2K Numeric burst (noQueue)
+	void b_2K_nq() {
+		EIcore_runner runner(false);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTest(ei.get(), 2*1000, 1);
+	}
+
+	// 2K Numeric burst (noQueue, not concurrent)
+	void b_2K_nqnc() {
+		EIcore_runner runner(false, false);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTest(ei.get(), 2*1000, 1);
+	}
+
+	// 2K Numeric burst (1-thread)
+	void b_2K_1t() {
+		EIcore_runner runner((size_t)1);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTest(ei.get(), 2*1000, 1);
+	}
+
+	// 2K Numeric burst (2-thread)
+	void b_2K_2t() {
+		EIcore_runner runner((size_t)2);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTest(ei.get(), 2*1000, 1);
+	}
+
+	// 2K Numeric burst (8-thread)
+	void b_2K_8t() {
+		EIcore_runner runner((size_t)8);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTest(ei.get(), 2*1000, 1);
+	}
+
+	// 500*100 Numeric Burst (0-thread)
+	void b_500b100_0t() {
+		EIcore_runner runner;
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTest(ei.get(), 500, 100);
+	}
+
+	// 500*100 Numeric Burst (1-thread)
+	void b_500b100_1t() {
+		EIcore_runner runner((size_t)1);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTest(ei.get(), 500, 100);
+	}
+
+	// 500*100 Numeric Burst (2-thread)
+	void b_500b100_2t() {
+		EIcore_runner runner((size_t)2);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTest(ei.get(), 500, 100);
+	}
+
+	// 500*100 Numeric Burst (8-thread)
+	void b_500b100_8t() {
+		EIcore_runner runner((size_t)8);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTest(ei.get(), 500, 100);
+	}
+
+
+	// 500*100*1 Numeric Burst (0-thread)
+	void b_500b100t1_0t() {
+		EIcore_runner runner((size_t)0);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTestAsync(ei.get(), 500, 100, 1);
+	}
+
+
+	// 500*10*10 Numeric Burst (0-thread)
+	void b_500b10t10_0t() {
+		EIcore_runner runner((size_t)0);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTestAsync(ei.get(), 500, 10, 10);
+	}
+
+	// 500*10*10 Numeric Burst (1-thread)
+	void b_500b10t10_1t() {
+		EIcore_runner runner((size_t)1);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTestAsync(ei.get(), 500, 10, 10);
+	}
+
+	// 500*10*10 Numeric Burst (2-thread)
+	void b_500b10t10_2t() {
+		EIcore_runner runner((size_t)2);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTestAsync(ei.get(), 500, 10, 10);
+	}
+
+	// 500*10*10 Numeric Burst (8-thread)
+	void b_500b10t10_8t() {
+		EIcore_runner runner((size_t)8);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTestAsync(ei.get(), 500, 10, 10);
+	}
+
+
+	// 50*10*100 Numeric Burst (0-thread)
+	void b_50b10t100_0t() {
+		EIcore_runner runner((size_t)0);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTestAsync(ei.get(), 50, 10, 100);
+	}
+
+	// 50*10*100 Numeric Burst (1-thread)
+	void b_50b10t100_1t() {
+		EIcore_runner runner((size_t)1);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTestAsync(ei.get(), 50, 10, 100);
+	}
+
+	// 50*10*100 Numeric Burst (2-thread)
+	void b_50b10t100_2t() {
+		EIcore_runner runner((size_t)2);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTestAsync(ei.get(), 50, 10, 100);
+	}
+
+	// 50*10*100 Numeric Burst (8-thread)
+	void b_50b10t100_8t() {
+		EIcore_runner runner((size_t)8);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTestAsync(ei.get(), 50, 10, 100);
+	}
+
+	// 50*100*100 Numeric Burst (0-thread)
+	void b_50b100t100_0t() {
+		EIcore_runner runner((size_t)0);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTestAsync(ei.get(), 50, 100, 100);
+	}
+
+	// 50*100*100 Numeric Burst (1-thread)
+	void b_50b100t100_1t() {
+		EIcore_runner runner((size_t)1);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTestAsync(ei.get(), 50, 100, 100);
+	}
+
+	// 50*100*100 Numeric Burst (2-thread)
+	void b_50b100t100_2t() {
+		EIcore_runner runner((size_t)2);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTestAsync(ei.get(), 50, 100, 100);
+	}
+
+	// 50*100*100 Numeric Burst (8-thread)
+	void b_50b100t100_8t() {
+		EIcore_runner runner((size_t)8);
+		std::unique_ptr<ExecutionInterface> ei(runner.createInterface());
+		numericBurstTestAsync(ei.get(), 50, 100, 100);
+	}
+
+};
+
+METHOD_AS_TEST_CASE ( RunnerTest::b_2K_0t, "2K Numeric Burst (0-thread)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_2K_nq, "2K Numeric burst (noQueue)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_2K_nqnc, "2K Numeric burst (noQueue, not concurrent)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_2K_1t, "2K Numeric burst (1-thread)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_2K_2t, "2K Numeric burst (2-thread)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_2K_8t, "2K Numeric burst (8-thread)", "[class]" )
+
+METHOD_AS_TEST_CASE ( RunnerTest::b_500b100_0t, "500*100 Numeric Burst (0-thread)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_500b100_1t, "500*100 Numeric Burst (1-thread)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_500b100_2t, "500*100 Numeric Burst (2-thread)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_500b100_8t, "500*100 Numeric Burst (8-thread)", "[class]" )
+
+METHOD_AS_TEST_CASE ( RunnerTest::b_500b100t1_0t, "500*100*1 Numeric Burst (0-thread)", "[class]" )
+
+METHOD_AS_TEST_CASE ( RunnerTest::b_500b10t10_0t, "500*10*10 Numeric Burst (0-thread)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_500b10t10_1t, "500*10*10 Numeric Burst (1-thread)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_500b10t10_2t, "500*10*10 Numeric Burst (2-thread)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_500b10t10_8t, "500*10*10 Numeric Burst (8-thread)", "[class]" )
+
+METHOD_AS_TEST_CASE ( RunnerTest::b_50b10t100_0t, "50*10*100 Numeric Burst (0-thread)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_50b10t100_1t, "50*10*100 Numeric Burst (1-thread)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_50b10t100_2t, "50*10*100 Numeric Burst (2-thread)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_50b10t100_8t, "50*10*100 Numeric Burst (8-thread)", "[class]" )
+
+METHOD_AS_TEST_CASE ( RunnerTest::b_50b100t100_0t, "50*100*100 Numeric Burst (0-thread)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_50b100t100_1t, "50*100*100 Numeric Burst (1-thread)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_50b100t100_2t, "50*100*100 Numeric Burst (2-thread)", "[class]" )
+METHOD_AS_TEST_CASE ( RunnerTest::b_50b100t100_8t, "50*100*100 Numeric Burst (8-thread)", "[class]" )
