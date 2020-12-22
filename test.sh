8e7013d6c8cd2464c107d4a7f4c441c2074ed966
XLP <1st@xlp-now.de>
2020-12-22 14:03:22 +0100

    Updated test-script

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-if cmake --build build --config Debug --target torasu-cpp-test; then
+if cmake --build build -j --config Debug --target torasu-cpp-test; then
+	echo "Running tests..."
 	./build/torasu-cpp-test
 else 
 	echo "Build of tests failed!"
