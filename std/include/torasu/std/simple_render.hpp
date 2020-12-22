e02799d0ccb29e8df472efb3a98ea5abf4305e5d
XLP <1st@xlp-now.de>
2020-12-22 16:06:40 +0100

    simple_render: validation/specific shortcuts

--- std/include/torasu/std/simple_render.hpp
+++ std/include/torasu/std/simple_render.hpp
@@ -3,9 +3,13 @@
 
 #include <memory>
 #include <string>
+#include <iostream>
+#include <sstream>
 
 #include <torasu/render_tools.hpp>
 #include <torasu/std/EIcore_runner.hpp>
+#include <torasu/std/Dnum.hpp>
+#include <torasu/std/Dstring.hpp>
 
 namespace torasu::tstd {
 
@@ -15,6 +19,20 @@ template<class T> struct SimpleResult {
 	torasu::tools::CastedRenderSegmentResult<T> rs;
 	torasu::ResultSegmentStatus segStat;
 	T* result;
+
+	bool check() {
+		return result != nullptr &&
+			   rStat == torasu::ResultStatus::ResultStatus_OK &&
+			   segStat == torasu::ResultSegmentStatus::ResultSegmentStatus_OK;
+	}
+
+	std::string getInfo() {
+		std::stringstream ss;
+		ss << "RSTAT=" <<  std::to_string(rStat) << ", "
+		   "SEGSTAT=" <<  std::to_string(segStat) << ", "
+		   "RES=" << result;
+		return ss.str();
+	}
 };
 
 template<class T> SimpleResult<T> simpleRender(Renderable* tree, std::string pl, torasu::ResultFormatSettings* format) {
@@ -40,6 +58,29 @@ template<class T> SimpleResult<T> simpleRender(Renderable* tree, std::string pl,
 
 	torasu::tools::CastedRenderSegmentResult<T> found = handle.getFrom(rr);
 	return {std::shared_ptr<RenderResult>(rr), rr->getStatus(), found, found.getStatus(), found.getResult()};
+}
+
+template<class T> SimpleResult<T> simpleRenderChecked(Renderable* tree, std::string pl, torasu::ResultFormatSettings* format) {
+
+	auto srr = simpleRender<T>(tree, pl, format);
+
+	if (srr.result == nullptr)
+		throw std::runtime_error(std::string() + "No result was generated! (" + srr.getInfo() + ")");
+
+	if (!srr.check()) {
+		std::cerr << "SimpleRender: The generated result may contain errors! (" << srr.getInfo() << ")" << std::endl;
+	}
+
+	return srr;
+}
+
+inline torasu::tstd::Dnum renderNum(Renderable* tree) {
+	return *simpleRenderChecked<torasu::tstd::Dnum>(tree, TORASU_STD_PL_NUM, nullptr).result;
+}
+
+
+inline torasu::tstd::Dstring renderString(Renderable* tree) {
+	return *simpleRenderChecked<torasu::tstd::Dstring>(tree, TORASU_STD_PL_STRING, nullptr).result;
 }
 
 } // namespace torasu::tstd
