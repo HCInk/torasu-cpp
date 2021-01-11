a9391454fcf39c55f61172a6b8972e74d10c6b34
XLP <1st@xlp-now.de>
2021-01-11 13:13:57 +0100

    Added speciifc linting-exclusion for implicit

--- lint.sh
+++ lint.sh
@@ -2,4 +2,25 @@
 files=$(cat .ci/sourcelist.txt | tr "\n" " ") # Read files to lint
 filters=$(grep -o '^[^#]*' .ci/cpplintfilter.txt | tr "\n" ",") # read filters with periods instead of newlines
 filters=${filters// } # remove whitespaces
-cpplint --extensions=cpp,cxx,cc,c,c++ --headers=h,hpp,hxx --filter=$filters $files # lint with given filters
+
+echo "// Ignored errors when linting " > .ci/cpplint-ignored-errors.txt
+# lint with given filters
+cpplint --extensions=cpp,cxx,cc,c,c++ --headers=h,hpp,hxx --filter=$filters $files \
+2> >(while read -r line ; do
+	if [[ $line == *"[runtime/explicit]"* ]]; then
+		srcLine=`sed -n "$(cut -f 2 -d : <<< $(echo $line))p" $(cut -f 1 -d ":" <<< $(echo $line))`
+
+		if [[ $srcLine == *"/* implciit */"* ]]; then
+			echo "$srcLine     // $line" >> .ci/cpplint-ignored-errors.txt
+		else
+    		>&2 echo "$line"
+		fi
+	else
+    	>&2 echo "$line"
+	fi
+
+done)
+
+ignored=`wc -l .ci/cpplint-ignored-errors.txt | cut -f 1 -d " "`
+((ignored--))
+echo "(Ignored $ignored)"
