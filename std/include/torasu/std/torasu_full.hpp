bb7f9ccc8f85c0aed49b77bf3d71ee111d0b4074
XLP <1st@xlp-now.de>
2021-03-13 17:30:36 +0100

    Add torasu_full header

---
+++ std/include/torasu/std/torasu_full.hpp
@@ -0,0 +1,66 @@
+#ifndef STD_INCLUDE_TORASU_STD_TORASU_FULL_HPP_
+#define STD_INCLUDE_TORASU_STD_TORASU_FULL_HPP_
+
+// CORE HEADERS
+#include <torasu/torasu.hpp>
+#include <torasu/json.hpp>
+
+#include <torasu/log_tools.hpp>
+#include <torasu/render_tools.hpp>
+#include <torasu/slot_tools.hpp>
+
+#include <torasu/DataPackable.hpp>
+#include <torasu/RenderableProperties.hpp>
+#include <torasu/SimpleRenderable.hpp>
+
+#include <torasu/Dlog_entry.hpp>
+
+// STD NAMES
+#include <torasu/std/context_names.hpp>
+#include <torasu/std/pipeline_names.hpp>
+
+// STD TOOLS
+#include <torasu/std/simple_render.hpp>
+
+// STD INTERFACES
+#include <torasu/std/EIcore_runner.hpp>
+#include <torasu/std/LIcore_logger.hpp>
+
+// STD DATA-TYPES
+#include <torasu/std/Dnum.hpp>
+#include <torasu/std/Dstring.hpp>
+#include <torasu/std/Dstring_map.hpp>
+#include <torasu/std/Dstring_pair.hpp>
+#include <torasu/std/Dfile.hpp>
+#include <torasu/std/Dbimg.hpp>
+#include <torasu/std/Dbimg_sequence.hpp>
+#include <torasu/std/Daudio_buffer.hpp>
+
+// STD RENDERABLES
+#include <torasu/std/Rlog_message.hpp>
+#include <torasu/std/Rerror.hpp>
+#include <torasu/std/Rproperty.hpp>
+#include <torasu/std/Rmix_pipelines.hpp>
+#include <torasu/std/Rfallback.hpp>
+#include <torasu/std/Rrctx_value.hpp>
+#include <torasu/std/Rmod_rctx.hpp>
+
+#include <torasu/std/Rnum.hpp>
+#include <torasu/std/Radd.hpp>
+#include <torasu/std/Rsubtract.hpp>
+#include <torasu/std/Rmultiply.hpp>
+#include <torasu/std/Rdivide.hpp>
+#include <torasu/std/Rfloor_mod.hpp>
+#include <torasu/std/Rsin.hpp>
+
+#include <torasu/std/Rstring.hpp>
+#include <torasu/std/Rstring_concat.hpp>
+#include <torasu/std/Rstring_map.hpp>
+#include <torasu/std/Rstring_replace.hpp>
+
+#include <torasu/std/Rlocal_file.hpp>
+#include <torasu/std/Rnet_file.hpp>
+#include <torasu/std/Rstring_file.hpp>
+#include <torasu/std/Rjson_prop.hpp>
+
+#endif // STD_INCLUDE_TORASU_STD_TORASU_FULL_HPP_
